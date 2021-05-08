dfaa250255681b676b93717db890a5aa86865939
Rafael Varago <varago.rafael@gmail.com>
2021-05-08 14:59:55 +0200

    Use map_err instead of ? in round robin

--- src/balancing/middleware/selector/cyclical.rs
+++ src/balancing/middleware/selector/cyclical.rs
@@ -38,23 +38,75 @@ impl Selector for RoundRobin {
     async fn select(&self) -> Result<Option<SocketAddr>, Self::Error> {
         let (tx, rx) = oneshot::channel();
 
-        self.msgs.send(Message { cb: tx }).await?;
+        self.msgs
+            .send(Message { cb: tx })
+            .await
+            .map_err(RoundRobinError::RequestBackendInfo)?;
 
-        Ok(rx.await?)
+        rx.await.map_err(RoundRobinError::ReceiveBackendInfo)
     }
 }
 
 #[derive(Debug, Error)]
 pub enum RoundRobinError {
     #[error("unable to request backend info")]
-    RequestBackendInfo(#[from] mpsc::error::SendError<Message>),
+    RequestBackendInfo(#[source] mpsc::error::SendError<Message>),
     #[error("unable to receive backend info")]
-    ReceiveBackendInfo(#[from] oneshot::error::RecvError),
+    ReceiveBackendInfo(#[source] oneshot::error::RecvError),
 }
 
 async fn handle_messages(backends: Vec<SocketAddr>, mut rx: mpsc::Receiver<Message>) {
     let mut backends = backends.into_iter().cycle();
     while let Some(msg) = rx.recv().await {
         let _ = msg.cb.send(backends.next());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use futures::{stream, StreamExt};
+    use std::net::Ipv4Addr;
+
+    #[tokio::test]
+    async fn select_returns_no_backend_when_an_empty_sequence_was_provided() {
+        // Pre-condition.
+        let backends = vec![];
+
+        // Action.
+        let round_robin = RoundRobin::with_backends(backends);
+
+        let selected_backend = round_robin.select().await.unwrap();
+
+        // Post-condition.
+        assert_eq!(selected_backend, None)
+    }
+
+    #[tokio::test]
+    async fn select_returns_backends_cyclically() {
+        // Pre-condition.
+        let backend_a = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 70);
+        let backend_b = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 80);
+        let backend_c = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 90);
+
+        let backends = vec![backend_a, backend_b, backend_c];
+
+        // Action.
+        let round_robin = RoundRobin::with_backends(backends);
+
+        let selected_backends = stream::iter(0..6)
+            .then(|_| round_robin.select())
+            .collect::<Vec<_>>()
+            .await
+            .into_iter()
+            .filter_map(Result::ok)
+            .flatten()
+            .collect::<Vec<_>>();
+
+        // Post-condition.
+        assert_eq!(
+            selected_backends,
+            vec![backend_a, backend_b, backend_c, backend_a, backend_b, backend_c]
+        )
     }
 }
