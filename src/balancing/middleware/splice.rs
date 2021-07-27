055a684d83bdbd9c8c2e8786805c25d3b9b0817b
Rafael Varago <varago.rafael@gmail.com>
2021-07-27 19:14:53 +0200

    Simplify manual splicing with tokio::io::copy_bidirectional

--- src/balancing/middleware/splice.rs
+++ src/balancing/middleware/splice.rs
@@ -3,7 +3,7 @@
 use super::{Pipe, PipeError};
 use anyhow::Context;
 use async_trait::async_trait;
-use tokio::io::{self, AsyncRead, AsyncWrite, AsyncWriteExt};
+use tokio::io::{copy_bidirectional, AsyncRead, AsyncWrite};
 use tracing::trace;
 
 /// A splicer for traffic between two streams.
@@ -18,28 +18,20 @@ pub fn splice() -> Splice {
 #[async_trait]
 impl<Sl, Sr> Pipe<(Sl, Sr)> for Splice
 where
-    Sl: AsyncRead + AsyncWrite + Send + 'static,
-    Sr: AsyncRead + AsyncWrite + Send + 'static,
+    Sl: AsyncRead + AsyncWrite + Send + Unpin + 'static,
+    Sr: AsyncRead + AsyncWrite + Send + Unpin + 'static,
 {
     type Output = ();
 
     async fn through(&self, src: (Sl, Sr)) -> Result<Self::Output, PipeError> {
         trace!("splicing streams");
 
-        let (mut ri, mut wi) = io::split(src.0);
-        let (mut ro, mut wo) = io::split(src.1);
+        let mut inbound = src.0;
+        let mut outbound = src.1;
 
-        tokio::try_join!(
-            async {
-                io::copy(&mut ri, &mut wo).await?;
-                wo.shutdown().await
-            },
-            async {
-                io::copy(&mut ro, &mut wi).await?;
-                wi.shutdown().await
-            }
-        )
-        .context("unable to splice streams")?;
+        copy_bidirectional(&mut inbound, &mut outbound)
+            .await
+            .context("unable to splice streams")?;
 
         trace!("spliced streams");
 
